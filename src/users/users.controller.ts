08792c8d2864f4d5f26af7a98c4de73a41dd7688
unlomtrois <vadim.kuz02@gmail.com>
2023-10-30 19:24:02 +0300

    docs: deprecate user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,7 +10,7 @@ import {
 import { UserService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Users')
 @Controller('users')
@@ -18,31 +18,37 @@ export class UsersController {
   constructor(private readonly usersService: UserService) {}
 
   @Post()
+  @ApiOperation({ deprecated: true })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({ deprecated: true })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get('id/:id')
+  @ApiOperation({ deprecated: true })
   findOneById(@Param('id') id: string) {
     return this.usersService.findOneBy({ id });
   }
 
   @Get('email/:email')
+  @ApiOperation({ deprecated: true })
   findOneByEmail(@Param('email') email: string) {
     return this.usersService.findOneBy({ email });
   }
 
   @Patch(':id')
+  @ApiOperation({ deprecated: true })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ deprecated: true })
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
